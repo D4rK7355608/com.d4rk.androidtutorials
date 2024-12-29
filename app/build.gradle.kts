990bee2d47ac24dec2e33662e4b5a5bb8f0e7f4c
Mihai-Cristian Condrea <mihai.condrea@digisoftware.ro>
2024-12-29 16:12:38 +0200

    docs: Updated app string resources
    Updated string resources for better clarity, consistency, and localization.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.d4rk.androidtutorials"
         minSdk = 23
         targetSdk = 35
-        versionCode = 104
+        versionCode = 105
         versionName = "1.1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resourceConfigurations += listOf(
